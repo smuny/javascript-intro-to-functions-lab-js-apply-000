64571eaa5261aaadd03aa3cb4c4249e8fafe5c0a
Sharon Mun <sharonymun@gmail.com>
2018-05-01 19:50:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  console.log(string === string.toLowerCase());
+  string === string.toLowerCase();
   if (string.toLowerCase){
     console.log('hello');
   }
