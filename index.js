de12c101f25e2ae44e1b41e3c2e291a7d4dca502
Sharon Mun <sharonymun@gmail.com>
2018-05-01 20:01:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
-function sayHiToGrandma(string){
-  if (string === string.toLowerCase())
+function sayHiToGrandma(string) {
+    if (string === string.toLowerCase());
     console.log('hello');
-  } else {
+    } else {
        console.log('I can\'t hear you!');
 }
       
